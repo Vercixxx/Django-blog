a935de0294f39a5251b1adef614004affa842a94
Vercixxx <krz.sluzalek@gmail.com>
2023-07-11 18:08:50 +0200

    Added table Comment and constraints

--- MyBlog/comments/models.py
+++ MyBlog/comments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from posts.models import Post
 
 # Create your models here.
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.TextField(default = "User")
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, to_field='id')
+    likes = models.IntegerField()
+    content = models.TextField()
+
+    class Meta:
+        db_table = 'Comment'
+        
+        
+class Posts_comments(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, to_field='id')
+    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE, to_field='id')
+    
+    class Meta:
+        db_table = 'Posts_comments'
--- MyBlog/posts/models.py
+++ MyBlog/posts/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class Post(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     author = models.TextField(default = "User")
     title = models.TextField()
     content = models.TextField()
     views = models.IntegerField()
     comments = models.IntegerField()
+
+    class Meta:
+        db_table = 'Post'
