e0bf8af3554afef835cb30a7e20d7409925ca3cc
Vercixxx <krz.sluzalek@gmail.com>
2023-07-13 19:40:34 +0200

    Add author and posted_date in post

--- MyBlog/posts/models.py
+++ MyBlog/posts/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
-    author = models.TextField(default = "User")
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_date = models.DateTimeField(default=timezone.now)
+    
     title = models.TextField()
     content = models.TextField()
+    
     views = models.IntegerField(default=0)
     comments = models.IntegerField(default=0)
 
