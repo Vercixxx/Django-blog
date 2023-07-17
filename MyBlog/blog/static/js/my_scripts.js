2bb26f2231d796e40ace5f70be2cb18e64b8a2a3
Vercixxx <krz.sluzalek@gmail.com>
2023-07-17 16:45:40 +0200

    Added email field to register form

--- MyBlog/blog/static/js/my_scripts.js
+++ MyBlog/blog/static/js/my_scripts.js
@@ -1,12 +1,9 @@
-const toastTrigger = document.getElementById('liveToastBtn');
-const toastLiveExample = document.getElementById('liveToast');
-const form = document.getElementById('myForm');
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
 
 if (toastTrigger) {
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
-  toastTrigger.addEventListener('click', (event) => {
-    event.preventDefault(); // Zatrzymaj domyślne zachowanie przeglądarki
-    toastBootstrap.show();
-    form.submit(); // Wyślij formularz ręcznie
-  });
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
 }
--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 from datetime import datetime
 
 # register
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from users.forms import UserRegisterForm
 
 # models
 from posts.models import Post 
@@ -18,22 +18,31 @@ def home_view(request):
 
 # register section:
     if request.method == 'POST':
-        register_form = UserCreationForm(request.POST)
+        register_form = UserRegisterForm(request.POST)
         
         if register_form.is_valid():
+            register_form.save()
+            
+            
             register_username = register_form.cleaned_data.get('username')
             messages.success(request, f'Account created for {register_username}!')
             # return redirect('home_view')
             
     else:
-        register_form = UserCreationForm()
+        register_form = UserRegisterForm()
     
+    
+
     
     data = {'posts':Post.objects.all(), 
             'Comments':Comment, 
             'timezone':timezone, 
             'datetime':datetime,
-            'register_form':UserCreationForm}
+            'register_form':register_form}
     
     return render(request, 'blog/blog.html', data)
+
+
+
+
 
