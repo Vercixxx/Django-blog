270838e00de370ae38e00c8fc107ddd8992ec09c
Vercixxx <krz.sluzalek@gmail.com>
2023-08-02 17:37:19 +0200

    Added search bar and logic to handle searches, searching in titles and contents

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -13,11 +13,24 @@ from comments.models import Comment
 # Pagination
 from django.core.paginator import Paginator
 
+# DB
+from django.db.models import Q
+
 
 def home_view(request): 
-
-    # Pagination
+        
+    searching = None
     all_posts = Post.objects.order_by('-posted_date')
+    
+    
+    # Search bar logic:
+    if request.method == 'POST'  and 'search_content' in request.POST:
+        searching = request.POST['search_content']
+
+        all_posts = all_posts.filter(Q(title__icontains=searching) | Q(content__icontains=searching))
+        
+	
+	# Pagination
     p = Paginator(all_posts, 5)
     
     page_number = request.GET.get('page')
@@ -30,6 +43,7 @@ def home_view(request):
             'timezone':timezone, 
             'datetime':datetime,
             'thumbs':PostsThumbs,
+            'searched':searching
             }
     
     return render(request, 'blog/blog.html', data)
