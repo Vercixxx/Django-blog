7ddf5fd8e240b7324da0bc36cd5ab6c781ccf260
Vercixxx <krz.sluzalek@gmail.com>
2023-08-23 15:33:08 +0200

    Added model for projects

--- MyBlog/projects/models.py
+++ MyBlog/projects/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Projects(models.Model):
+    id = models.AutoField(primary_key=True)
+    
+    posted_date = models.DateTimeField(default=timezone.now)
+    
+    title = models.TextField()
+    content = models.TextField()
+    
+    status = models.IntegerField(default=0)
+    
+    views = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'Projects'
+        
--- MyBlog/projects/views.py
+++ MyBlog/projects/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Projects
 
 
 def projects_view(request):
-    return render(request, 'projects.html')
+    projects = Projects.objects.order_by('-posted_date')
+    
+    output = {
+        'projects' : projects
+    }
+    
+    return render(request, 'projects/projects.html', output)
