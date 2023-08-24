10f70212292d239e293b601406979b308feae636
Vercixxx <krz.sluzalek@gmail.com>
2023-08-24 22:12:22 +0200

    Hide secret_key

--- MyBlog/MyBlog/settings.py
+++ MyBlog/MyBlog/settings.py
@@ -15,7 +15,7 @@ import os
 
 # My imports
 # secret_data
-from users.import_data import json_data
+from users.import_data import json_data, SECRET_KEY_VALUE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3#a1v81dwrg7jketq*0590x9++miyws#lwj)e5d=wl!%u^xgk-'
+SECRET_KEY = SECRET_KEY_VALUE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
